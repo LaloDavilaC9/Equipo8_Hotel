7f58e8b8ce4906b264f8dc9d5168c7bff99435a7
Brayan-ux <Bsanchez6941@gmail.com>
2021-05-06 12:56:31 -0500

    Borre linea
    Elimine la línea que escribió Victor

--- HotelEquipo8/src/hotelequipo8/HotelEquipo8.java
+++ HotelEquipo8/src/hotelequipo8/HotelEquipo8.java
@@ -5,7 +5,6 @@ public class HotelEquipo8 {
 
     public static void main(String[] args) {
         System.out.println("Hola, esta es la linea base");
-        System.out.println("Hola, este es mi cambio. Victor.");
     }
     
 }
