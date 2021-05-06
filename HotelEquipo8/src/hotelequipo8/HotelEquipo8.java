ab3401bb2adff853ad2f6188f8da2db30dc3f73d
VictorLuna-ISC <victor_delgado1369@hotmail.com>
2021-05-06 12:53:17 -0500

    SOUT Hola victor.
    Agregue una linea de codigo con un saludo.

--- HotelEquipo8/src/hotelequipo8/HotelEquipo8.java
+++ HotelEquipo8/src/hotelequipo8/HotelEquipo8.java
@@ -5,6 +5,7 @@ public class HotelEquipo8 {
 
     public static void main(String[] args) {
         System.out.println("Hola, esta es la linea base");
+        System.out.println("Hola, este es mi cambio. Victor.");
     }
     
 }
