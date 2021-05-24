2382c86f12397b62f4e5039b619d5aff56471d26
VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>
2021-05-24 12:13:40 -0500

    Creacion de pantallas.
    -Se añadio una pantalla de inicio en donde se solicitan  el nombre de usuario y la contraseña respectivamente.
    -Tambien se agrego el boton de galeria dentro de la pantalla principal con un conjunto de fotos del hote, tipos de habitaciones y amenidades.
    -Dentro del boton de galeria encontramos un conjunto de fotos junto con un boton de "salir" para regresar a la pantalla principal, un "siguiente" para la siguiente foto y un boton "anterior" para la foto de atras respectivamente.

---
+++ HotelEquipo8/src/victor/LlamadoPantallaPrincipal.java
@@ -0,0 +1,10 @@
+
+package victor;
+
+public class LlamadoPantallaPrincipal {
+        public static void main(String[] args) {
+        // TODO code application logic here
+        PantallaPrincipal P = new PantallaPrincipal();
+        P.setVisible(true);
+    }
+}
