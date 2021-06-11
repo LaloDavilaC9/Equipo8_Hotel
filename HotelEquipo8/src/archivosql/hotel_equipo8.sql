67ed567d701b5e687c0399bcdf9a76c797a8c4f5
VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>
2021-06-10 21:31:41 -0500

    Otra nueva BDD.
    Co-Authored-By: LuisBA31 <74994999+LuisBA31@users.noreply.github.com>
    Co-Authored-By: Brayan GS <56101054+Brayan-ux@users.noreply.github.com>

--- HotelEquipo8/src/archivosql/hotel_equipo8.sql
+++ HotelEquipo8/src/archivosql/hotel_equipo8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2021 a las 04:23:40
+-- Tiempo de generación: 11-06-2021 a las 04:31:13
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.6
 
@@ -20,6 +20,93 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `hotel_equipo8`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuentas`
+--
+
+CREATE TABLE `cuentas` (
+  `usuario` varchar(45) NOT NULL,
+  `contrasenia` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cuentas`
+--
+
+INSERT INTO `cuentas` (`usuario`, `contrasenia`) VALUES
+('Luis31', '872488f88d1b2db54d55bc8bba2fad1b'),
+('VictorLuna13', '17a821dfa961c93a6c586ca257750fb2'),
+('LaloDavila', 'a4849ea5705eaa21162b7338ee2e9e32'),
+('BrayanGaytan', 'b3fb45d7cdac3ff5babbdde65e7c8427'),
+('GinaSalazar', 'fa0e62ff4c88ad384523d595cb1ba738');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitaciones`
+--
+
+CREATE TABLE `habitaciones` (
+  `Hab_id` int(10) NOT NULL,
+  `Tipo_id` int(2) NOT NULL,
+  `Piso` int(2) NOT NULL,
+  `Disponible` int(2) NOT NULL,
+  `ocupantes` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `habitaciones`
+--
+
+INSERT INTO `habitaciones` (`Hab_id`, `Tipo_id`, `Piso`, `Disponible`, `ocupantes`) VALUES
+(101, 1, 1, 0, 3),
+(102, 1, 1, 0, 2),
+(103, 1, 1, 0, 2),
+(104, 1, 1, 0, 1),
+(105, 1, 1, 0, 1),
+(106, 2, 1, 1, 0),
+(107, 2, 1, 1, 0),
+(108, 2, 1, 1, 0),
+(109, 2, 1, 1, 0),
+(110, 2, 1, 1, 0),
+(111, 2, 1, 1, 0),
+(112, 3, 1, 1, 0),
+(113, 3, 1, 1, 0),
+(114, 3, 1, 1, 0),
+(115, 3, 1, 1, 0),
+(201, 1, 2, 0, 1),
+(202, 1, 2, 0, 1),
+(203, 1, 2, 0, 2),
+(204, 2, 2, 1, 0),
+(205, 2, 2, 1, 0),
+(206, 2, 2, 1, 0),
+(207, 2, 2, 1, 0),
+(208, 2, 2, 1, 0),
+(209, 2, 2, 1, 0),
+(210, 2, 2, 0, 2),
+(211, 3, 2, 0, 2),
+(212, 3, 2, 1, 0),
+(213, 3, 2, 1, 0),
+(214, 3, 2, 1, 0),
+(215, 3, 2, 1, 0),
+(301, 1, 3, 1, 0),
+(302, 1, 3, 0, 1),
+(303, 1, 3, 1, 0),
+(304, 1, 3, 1, 0),
+(305, 1, 3, 0, 1),
+(306, 1, 3, 1, 0),
+(307, 2, 3, 1, 0),
+(308, 2, 3, 1, 0),
+(309, 2, 3, 1, 0),
+(310, 3, 3, 0, 3),
+(311, 3, 3, 1, 0),
+(312, 3, 3, 1, 0),
+(313, 3, 3, 1, 0),
+(314, 3, 3, 1, 0),
+(315, 3, 3, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -60,9 +147,63 @@ INSERT INTO `huespedes` (`Huesped_id`, `Nombre`, `Ap_Paterno`, `Ap_Materno`, `Ci
 (39, 'Ricardo', 'Rodriguez', 'Almeria', 'Medellin', '2021-06-01', '2021-06-04', 202, 1, 1, 3),
 (40, 'Alberto', 'Gutierres', 'Davila', 'Aguascalientes', '2021-06-01', '2021-06-05', 203, 2, 1, 4);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `info_hotel`
+--
+
+CREATE TABLE `info_hotel` (
+  `nombre_hotel` varchar(40) NOT NULL,
+  `slogan_hotel` varchar(255) NOT NULL,
+  `direccion_hotel` varchar(255) NOT NULL,
+  `ingresos_hotel` float NOT NULL,
+  `hab_sencillas` int(10) NOT NULL,
+  `hab_dobles` int(10) NOT NULL,
+  `hab_triples` int(10) NOT NULL,
+  `imagenes` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `info_hotel`
+--
+
+INSERT INTO `info_hotel` (`nombre_hotel`, `slogan_hotel`, `direccion_hotel`, `ingresos_hotel`, `hab_sencillas`, `hab_dobles`, `hab_triples`, `imagenes`) VALUES
+('California Suite', 'La mejor experiencia, sólo en el mejor hotel', 'Avenida Cruza el Río, Fraccionamiento Las Hadas, Nuevo Vallarta, Nayarit. ', 4652.7, 14, 12, 11, 'src/imagenes/imagenesGaleria/imagenGaleria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_hab`
+--
+
+CREATE TABLE `tipos_hab` (
+  `Tipo_id` int(2) NOT NULL,
+  `Tipo_modelo` varchar(50) NOT NULL,
+  `Tipo_costo` float NOT NULL,
+  `Tipo_cargoExtra` float NOT NULL,
+  `Tipo_capacidad` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipos_hab`
+--
+
+INSERT INTO `tipos_hab` (`Tipo_id`, `Tipo_modelo`, `Tipo_costo`, `Tipo_cargoExtra`, `Tipo_capacidad`) VALUES
+(1, 'Sencilla', 1450.9, 500, 1),
+(2, 'Doble', 1795.8, 760, 2),
+(3, 'Triple', 2100.85, 945, 3);
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD PRIMARY KEY (`Hab_id`),
+  ADD KEY `Tipo_id` (`Tipo_id`);
 
 --
 -- Indices de la tabla `huespedes`
@@ -72,8 +213,20 @@ ALTER TABLE `huespedes`
   ADD KEY `No_Habitacion` (`No_Habitacion`);
 
 --
+-- Indices de la tabla `tipos_hab`
+--
+ALTER TABLE `tipos_hab`
+  ADD PRIMARY KEY (`Tipo_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  MODIFY `Hab_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=316;
 
 --
 -- AUTO_INCREMENT de la tabla `huespedes`
@@ -82,8 +235,20 @@ ALTER TABLE `huespedes`
   MODIFY `Huesped_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
+-- AUTO_INCREMENT de la tabla `tipos_hab`
+--
+ALTER TABLE `tipos_hab`
+  MODIFY `Tipo_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD CONSTRAINT `habitaciones_ibfk_1` FOREIGN KEY (`Tipo_id`) REFERENCES `tipos_hab` (`Tipo_id`);
 
 --
 -- Filtros para la tabla `huespedes`
